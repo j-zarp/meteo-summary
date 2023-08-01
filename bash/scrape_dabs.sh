72abdcd0fcf5fcf6c7f094d57abf59a9561c29cc
jeremie <20644775+jdespraz@users.noreply.github.com>
2023-08-01 22:21:14 +0200

    first code commit

---
+++ bash/scrape_dabs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O ../web/assets/pdf/dabs_today.pdf https://www.skybriefing.com/portal/delegate/dabs?today 
+wget -O ../web/assets/pdf/dabs_tomorrow.pdf https://www.skybriefing.com/o/dabs?tomorrow 
+
+
