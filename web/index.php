28e3f7f06b4841f6ac336868345334e5e8a1f5c6
Jérémie <20644775+j-zarp@users.noreply.github.com>
2024-08-09 00:04:46 +0200

    Update index.php with favicons

--- web/index.php
+++ web/index.php
@@ -19,10 +19,14 @@
   
   <link rel="stylesheet" media="all" href="./assets/css/style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
-  
+
+  <!-- headers for the favicon on various platforms -->
+  <link rel="shortcut icon" href="images/favicon.ico?v=2">
   <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io/favicon-16x16.png">
+  <link rel="icon" type="image/png" href="./favicon_io/android-chrome-192x192.png?v=2" sizes="192x192">
+  <link rel="icon" type="image/png" href="./favicon_io/android-chrome-512x512.png?v=2" sizes="512x512">
   <link rel="manifest" href="./favicon_io/site.webmanifest">
   
   <style> 
