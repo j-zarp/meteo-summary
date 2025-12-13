88b8e641d011b3c98926dd7b80cb323f5e99595f
Jérémie <20644775+j-zarp@users.noreply.github.com>
2025-12-13 20:10:36 +0100

    Adding meteosuisse wind predictions in the bulletin

--- web/index.php
+++ web/index.php
@@ -23,7 +23,28 @@
       rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
+    <!-- Favicons for various platforms -->
     <link rel="shortcut icon" href="/favicon_io/favicon.ico?v=2">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/favicon_io/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/favicon_io/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/favicon_io/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/favicon_io/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/favicon_io/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/favicon_io/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/favicon_io/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/favicon_io/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="/favicon_io/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="/favicon_io/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="/favicon_io/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/favicon_io/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/favicon_io/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="/favicon_io/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="/favicon_io/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="/favicon_io/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="/favicon_io/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="/favicon_io/mstile-310x310.png" />
     <link rel="manifest" href="/favicon_io/manifest.json">
 
     <style>
@@ -769,11 +790,9 @@
               <div class="iframe-wrapper">
                 <div class="iframe-overlay">
                 </div>
-                <iframe width="100%" height="650"
-                  src="https://embed.windy.com/embed2.html?lat=45.613&lon=9.406&detailLat=46.291&detailLon=7.539&width=650&height=650&zoom=2&level=surface&overlay=pressure&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=true&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1"
-                  allowfullscreen="true" allow="geolocation" frameborder="0" class="my-iframe"></iframe>
-                <a href="https://www.windy.com/-Pressure-pressure?pressure,47.364,8.544,5,i:pressure"
-                  target="_blank">windy</a>
+                <iframe width="100%" height="650" src="https://embed.windy.com/embed2.html?lat=45.613&lon=9.406&detailLat=46.291&detailLon=7.539&width=650&height=650&zoom=2&level=surface&overlay=pressure&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=true&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1"
+                  allowfullscreen="true" allow="geolocation" frameborder="0" class="my-iframe" loading="lazy"></iframe>
+                <a href="https://www.windy.com/-Pressure-pressure?pressure,47.364,8.544,5,i:pressure" target="_blank" rel="noopener">windy</a>
               </div>
             </div>
           </div>
